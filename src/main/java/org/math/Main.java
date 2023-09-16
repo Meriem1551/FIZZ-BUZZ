49ad974e33f97b1698090660a705bfbb33c01695
Meriem1551 <meriem200409@gmail.com>
2023-09-16 18:00:39 +0200

    update project

--- src/main/java/org/math/Main.java
+++ src/main/java/org/math/Main.java
@@ -1,7 +1,20 @@
 package org.math;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Number: ");
+        int number = scanner.nextInt();
+
+        if (number % 5 == 0 && number % 3 == 0)
+            System.out.println("FizzBuzz");
+        else if (number % 5 == 0)
+            System.out.println("Buzz");
+        else if (number % 3 == 0)
+            System.out.println("Fizz");
+        else
+            System.out.println(number);
     }
 }
